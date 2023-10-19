141ec2984ccf21b2bb40519e83b165094e2d3b3c
DarkNet403 <142100190+Darknet403@users.noreply.github.com>
2023-10-19 20:59:43 +0700

    cnc1.py

--- cyber.py
+++ cyber.py
@@ -1,5 +1,5 @@
-# Compile By AmmarrBN
-# https://github.com/AmmarrBN/Py-Fuscate
+# Compile By AmmarrBN && Mr.Cyber
+# https://github.com/teamvyber-glitch
 # Make Sure You're Running The Program With python3.11 Otherwise It May Crash
 # To Check Your Python Version Run "python -V" Command
 try:
